c992911ba48b95f28ec96fead90588d8ded4c403
MONISHSHARMA080 <monishsharma080@gmail.com>
2025-07-27 20:08:23 +0530

    done with the Qn4

--- ps1/src/featuremaps/featuremap.py
+++ ps1/src/featuremaps/featuremap.py
@@ -19,6 +19,7 @@ class LinearModel(object):
         self.theta = theta
         self.degree_of_poly = 1
         self.enable_logs = enable_logs
+        self.do_we_need_sin_in_phi = False
 
     def fit(self, X, Y, degree_of_polynomial=5, do_we_need_sin_in_phi:bool = False):
         """Run solver to fit linear model. You have to update the value of
@@ -53,15 +54,17 @@ class LinearModel(object):
         #
         X = self.create_poly_feature_map(degree_of_polynomial, X, do_we_need_sin_in_phi)
         print(F" created poly_feature_map X and it's shape is {X.shape} ")
-        theta = self.calculate_theta_with_normal_eqn(degree_of_polynomial, X, Y)
+        theta = self.calculate_theta_with_normal_eqn(degree_of_polynomial, X, Y, do_we_need_sin_in_phi)
         self.theta = theta
         print(F" the shape of theta after the fitting is {theta.shape} and X's shape is {X.shape} ")
         self.degree_of_poly = degree_of_polynomial
+        self.do_we_need_sin_in_phi = do_we_need_sin_in_phi
         # *** END CODE HERE ***
 
-    def calculate_theta_with_normal_eqn(self, degree_of_polynomial:int, X:np.ndarray, Y:np.ndarray, )->np.ndarray:
-        theta = np.zeros((degree_of_polynomial + 1,1))
-        print(F" the theta vec obtained is {theta}  and it's shape is {theta.shape}")
+    def calculate_theta_with_normal_eqn(self, degree_of_polynomial:int, X:np.ndarray, Y:np.ndarray, do_we_need_sin_in_phi:bool = False)->np.ndarray:
+        rows_in_theta = degree_of_polynomial + 2 if  do_we_need_sin_in_phi else degree_of_polynomial + 1
+        theta = np.zeros((rows_in_theta,1))
+        print(F" the theta vec obtained is {theta}  and it's shape is {theta.shape} and rows in theta is {rows_in_theta} and do_we_need_sin_in_phi:{do_we_need_sin_in_phi} and degree_of_polynomial is {degree_of_polynomial}")
         assert theta.shape[0] == X.shape[1], F" theta's row should be equal to the column of the X matrix, theta:{theta.shape}, X:{X.shape}"
         assert Y.shape[0] == X.shape[0] , F" the number of rows in the y should be same as the number of rows in the x(same as no. of eg.). X:{X.shape}, y:{Y.shape} "
         XT_X = np.dot(np.transpose(X), X)
@@ -90,11 +93,12 @@ class LinearModel(object):
         """
         # *** START CODE HERE ***
         number_0f_eg = input.shape[0]
-        dim_of_features = k+1
+        dim_of_features = k+1 if not do_we_need_sin_in_phi else k + 2
         print(F"the number_0f_eg is {number_0f_eg}, and dim_of_features {dim_of_features}")
         #final X matrix
         X = np.zeros(( number_0f_eg, dim_of_features))
         for i in range(number_0f_eg):
+            # print(F"at {i}: the x[{i}] is {X[i]} and it's shape {X[i].shape}")
             X[i] = self.phi(input[i], k, do_we_need_sin_in_phi)
             # print(F"\n\n"+"-"*40+F"{i}"+"-"*40+"\n\n")
             # print(F"{X[i]}")
@@ -152,7 +156,7 @@ class LinearModel(object):
             raise ValueError("the self.degree_of_poly is not of int type")
  
         print(F"-- in pred -- the shape of theta is {self.theta.shape} (it should be ({self.degree_of_poly+1},1)  )   and shape of X is {X.shape} ")
-        X_poly = self.create_poly_feature_map(self.degree_of_poly, X)
+        X_poly = self.create_poly_feature_map(self.degree_of_poly, X, do_we_need_sin_in_phi=self.do_we_need_sin_in_phi)
         res = np.dot( X_poly, self.theta )
         # res = np.dot( self.theta, X)
         print(F" the output from predict function has a shape {res.shape} \n\n")
@@ -195,7 +199,7 @@ def main(train_path, small_path, eval_path, degree_of_poly = 3):
     print(F" saved the files as:linear_reg_with_feature_map_with_poly_degree_{degree_of_poly}.jpg")
     # *** END CODE HERE ***
 
-def compare_diff_degree_poly( train_path:str, k_value , color, do_we_need_sin_in_phi:bool = False  ):
+def compare_diff_degree_poly( train_path:str, k_value , color, do_we_need_sin_in_phi:bool = False, extra_namePfor_saved_file = None  ):
     if len(k_value) != len(color):
         raise ValueError(F" the len of k value is not same as the no. of color , k_val:{len(k_value)}, color:{len(color)} ")
     
@@ -205,6 +209,7 @@ def compare_diff_degree_poly( train_path:str, k_value , color, do_we_need_sin_in
     plt.scatter(train_x, train_y, label='Training Data', )
 
     x_range = np.linspace(min(train_x), max(train_x), 1000).reshape(-1, 1)
+    saved_img_name = ""
 
     for k, color in zip(k_value, color):
         clf = LinearModel()
@@ -212,17 +217,23 @@ def compare_diff_degree_poly( train_path:str, k_value , color, do_we_need_sin_in
         clf.fit(train_x, train_y, degree_of_polynomial=k, do_we_need_sin_in_phi= do_we_need_sin_in_phi)
         y_pred = clf.predict(x_range)
         plt.plot(x_range, y_pred, color=color, label=f'k={k} Hypothesis')
+        saved_img_name+= "-"+str(k)
+
+    saved_img_name += str(extra_namePfor_saved_file)
 
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title('Polynomial Regression with Different Degrees')
     plt.legend()
-    plt.savefig('comparison-3-5-10-20.jpg')
-    print("\n Saved plot as comparison-3-5-10-20.jpg \n")
+
+    plt.savefig(f'comparison{saved_img_name}.jpg')
+    print(f"\n Saved plot as comparison{saved_img_name}.jpg \n")
 
 
 if __name__ == '__main__':
     # main(train_path='train.csv',
     #     small_path='small.csv',
     #     eval_path='test.csv', degree_of_poly=3)
-    compare_diff_degree_poly(train_path="train.csv", k_value=[0,1,2,3,5,10,20], color=['blue', 'green', 'red', 'purple', 'yellow', 'violet', 'orange'], do_we_need_sin_in_phi=True )
+    # compare_diff_degree_poly(train_path="train.csv", k_value=[0,1,2,3,5,10,20], color=['blue', 'green', 'red', 'purple', 'yellow', 'violet', 'black'], do_we_need_sin_in_phi=True, extra_namePfor_saved_file="_on_test" )
+    compare_diff_degree_poly(train_path="small.csv", k_value=[1,2,5,10,20], color=['blue', 'green', 'red', 'purple', 'yellow'], do_we_need_sin_in_phi=False, extra_namePfor_saved_file="_on_small_dtst" )
+    # compare_diff_degree_poly(train_path="train.csv", k_value=[3,5,10,20], color=['blue', 'green', 'red', 'purple'], do_we_need_sin_in_phi=True )
